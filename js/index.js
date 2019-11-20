35cec47865219d97d5d3c7e52e9ff100c4a67a3c
nickLuk <57568820+nickLuk@users.noreply.github.com>
2019-11-20 19:39:10 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,27 @@
-
 document.querySelector('button').addEventListener('click',load);
-function load(){
+var input = document.querySelector('input');
 
-    let url='http://www.omdbapi.com/?apikey=1e580308&t=Batman';
+function load(){
+    let name = input.value;
+    let url=`http://www.omdbapi.com/?i=tt3896198&apikey=1e580308&t=${name}`;
+    //var req = new Request(link);
       fetch(url)
       .then(
           function(response){
-              console.log(response);
+              
               return response.json();
           }
       )
       .then(
         
           function(data){
-              console.log(data.poster);
-            var poster = document.getElementById('poster');
-                                       
+              console.log(data.Poster)
+            let text=document.querySelector('h3')
+              text.innerHTML = data.Title;
+             let poster = document.getElementById('poster')
+            
+             poster.innerHTML =`<img src="${data.Poster}">`
+                                    
                 
                 
   
